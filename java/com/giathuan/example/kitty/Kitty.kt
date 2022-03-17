c0fa83d443ce19a2a0999e8f438a82d61d9efbe9
Gia Thuan Lam <duckladydinh@gmail.com>
2022-03-17 16:25:58 +0100

    CM0004 (main): Add example of subcommand.

--- java/com/giathuan/example/kitty/Kitty.kt
+++ java/com/giathuan/example/kitty/Kitty.kt
@@ -2,6 +2,7 @@ package com.giathuan.example.kitty
 
 import com.github.ajalt.clikt.completion.completionOption
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 
@@ -12,18 +13,30 @@ import com.github.ajalt.clikt.parameters.options.option
  * alias kitty=$(bazel info workspace)/bazel-bin/java/com/giathuan/example/kitty/kitty && bazel build -c opt java/com/giathuan/example/kitty:kitty && source <(kitty --generate-completion=bash)
  * ```
  */
-class Kitty : CliktCommand(name = "kitty") {
-    private val name by option(help = "Name of the kitty").default("Kitty")
-
+private class Kitty : CliktCommand(name = "kitty") {
     init {
-        this.completionOption()
+        completionOption()
+        subcommands(German(), English())
     }
 
-    override fun run() {
-        println("Hello, $name!")
+    override fun run() {}
+
+
+    class German : CliktCommand(name = "german") {
+        private val name by option(help = "Name of the kitty").default("Kitty")
+
+        override fun run() {
+            println("Hallo, $name!")
+        }
+    }
+
+    class English : CliktCommand(name = "english") {
+        private val name by option(help = "Name of the kitty").default("Kitty")
+
+        override fun run() {
+            println("Hello, $name!")
+        }
     }
 }
 
-fun main(vararg args: String) {
-    Kitty().main(args)
-}
+fun main(vararg args: String) = Kitty().main(args)
